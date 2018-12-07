d4fed3523ad7d129b14a52afb4090f3b757b6785
thatnewcarsmell <scalajr@gmail.com>
2018-12-07 11:08:43 -0700

    creating knex migration for the snacks schema

--- migrations/20181207105016_snacks.js
+++ migrations/20181207105016_snacks.js
@@ -1,8 +1,15 @@
 
 exports.up = function(knex, Promise) {
-  
+  return knex.schema.createTable('snacks', (snack) => {
+      snack.increments('id')
+      snack.string('name')
+      snack.string('description')
+      snack.boolean('is_perishable')
+      snack.string('price')
+      snack.string('img')
+  })
 };
 
 exports.down = function(knex, Promise) {
-  
+    return knex.schema.dropTableIfExists('snacks')
 };
