ec5d104838296af9df117e874fb0b313da1e07d5
thatnewcarsmell <scalajr@gmail.com>
2018-12-07 11:27:56 -0700

    adding review knex schema migrations, and small edits to snacks migration

--- migrations/20181207105016_snacks.js
+++ migrations/20181207105016_snacks.js
@@ -1,13 +1,13 @@
 
 exports.up = function(knex, Promise) {
-  return knex.schema.createTable('snacks', (snack) => {
-      snack.increments('id')
-      snack.string('name')
-      snack.string('description')
-      snack.boolean('is_perishable')
-      snack.string('price')
-      snack.string('img')
-  })
+    return knex.schema.createTable('snacks', (snack) => {
+        snack.increments('id')
+        snack.string('name')
+        snack.string('description')
+        snack.boolean('is_perishable')
+        snack.decimal('price', 6, 2)
+        snack.string('img')
+    })
 };
 
 exports.down = function(knex, Promise) {
